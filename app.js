13dbdde53ef8f4aef82b0178669418c864339cf3
mdakhib <mohamedakhib@gmail.com>
2020-09-16 17:29:08 +0530

    day and todo input added using EJS

--- app.js
+++ app.js
@@ -2,42 +2,35 @@ const express=require("express")
 const bodyParser=require("body-parser")
 const app=express()
 
-app.set('view engine', 'ejs');
 
+let items=["Khana","Peena","Sona"];
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}))
 app.get("/",(req,res)=>{
     
-    var today=new Date();
-    var currentDay=today.getDay();
-    var day ="";
-    
-    switch (currentDay) {
-        case 0:
-            day="Sunday";
-            break;
-        case 1:
-            day="Monday";
-            break;
-        case 2:
-            day="Tuesday";
-            break;
-        case 3:
-            day="Wednesday";
-            break;
-        case 4:
-            day="Thursday";
-            break;
-        case 5:
-            day="Friday";
-            break;
-        case 6:
-            day="Saturday";
-            break;
-    
-        default:
-            console.log("Error current day is equal to:"+ currentDay);
-    }
-    res.render("list",{kindOfDay:day});
+    let today=new Date();
+    let options={
+        weekday:"long",
+        day:"numeric",
+        month:"long",
+        hour:"numeric",
+        minute:"numeric"
+    };
+    let day=today.toLocaleDateString("en-US", options);
+
+    res.render("list",{
+        kindOfDay:day,
+        newListItems:items
+    });
 });
+
+app.post("/",(req,res)=>{
+    var item=req.body.newItem;
+    items.push(item)
+    res.redirect("/");
+})
 
 app.listen(3000,()=>{
     console.log("server is running at 3000");
