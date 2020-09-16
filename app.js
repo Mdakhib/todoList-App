f589259f12d4a9a036724af0386471de18d67360
mdakhib <mohamedakhib@gmail.com>
2020-09-16 19:14:12 +0530

    added workspace page by rendoring

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app=express()
 
 
 let items=["Khana","Peena","Sona"];
-
+let workItems=[]
 
 
 app.set('view engine', 'ejs');
@@ -23,15 +23,24 @@ app.get("/",(req,res)=>{
     let day=today.toLocaleDateString("en-US", options);
 
     res.render("list",{
-        kindOfDay:day,
+        listTitle:day,
         newListItems:items
     });
 });
 
 app.post("/",(req,res)=>{
-    var item=req.body.newItem;
-    items.push(item)
+    let item=req.body.newItem;
+    if(req.body.list==="Work"){
+        workItems.push(item);
+        res.redirect("/work")
+    }else{
+        items.push(item)
+    }
     res.redirect("/");
+})
+
+app.get("/work",(req,res)=>{
+    res.render("list",{listTitle:"Work List",newListItems:workItems});
 })
 
 app.listen(3000,()=>{
